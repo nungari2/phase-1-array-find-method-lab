abba98581daf1df6955ac0567fb5f4b54a51211c
ann nungari <annnjoroge98765@gmail.com>
2025-03-20 12:41:06 +0300

    iterate rray

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    for (let i = 0; i < record.length; i++) {
+      if (record[i].result === "W") {
+        return record[i].year;
+      }
+    }
+    return undefined;
+  }
